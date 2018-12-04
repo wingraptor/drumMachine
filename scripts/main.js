a1c69d330124af557892d603ae1e8f96827df5a7
wingraptor <akonobrathwaite@gmail.com>
2018-12-04 17:18:45 -0400

    Complet project

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,48 @@
 // Code to be passed to function which is executed once DOM content has been loaded
 var callback = function(){
-  // Select all child elements in #display div
-  var keys = document.querySelectorAll("#display > div");
+  function playSound(){
+    // convert keyCode from keypress event to letter
+    var letter = String.fromCharCode(event.keyCode);
+    // Select specific audio element
+    var audio = document.querySelector(`audio[id="${letter}"]`);
+    // Select specific drum-pad div
+    var pad = document.querySelector(`.drum-pad[data-key="${letter}"]`);
+    //Stop function if no corresponding audio element selected
+    if (!audio) return;
+    // reset audio playback position to start - allows audio to reset each time key is pressed
+    audio.currentTime = 0;
+    //Add class to drum-pad element
+    pad.classList.add("pressed");
+    // Change bg-color of .drum-pad to random color
+    pad.style.backgroundColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+    // Change bg-color of body to random color
+    document.querySelector("body").style.backgroundColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+    //play audio
+    audio.play();
+  }
 
-  // Add event listener to each item in #display div
-  keys.forEach(function (key) {
-    key.addEventListener("click", function () {
-      console.log("hello");
+  //Store all drum-pad elements on page
+  var pads = document.querySelectorAll(".drum-pad");
+
+  // Add event listener to each drum-pad
+  pads.forEach(function(pad){
+    pad.addEventListener("transitionend", function(event){
+      // Remove class when transition complete
+      this.classList.remove("pressed");
+      this.style.backgroundColor = "white";
+      // Set bg-color of body
+      document.querySelector("body").style.backgroundColor = `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
     });
   });
+
+  //Listen for keypress on document and play sound
+  document.addEventListener("keydown", playSound);
 }
 
+// Generate random number between 0 and 255
+function randomNum() {
+  return Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+}
 
 // Code executed only when page has been loaded
 function ready(callback) {
